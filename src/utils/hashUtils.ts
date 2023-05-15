3daae8b62ec2dbe4e041a8934c326299ffe41e53
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2023-05-15 16:23:55 +0200

    Add hashing utils

---
+++ src/utils/hashUtils.ts
@@ -0,0 +1,17 @@
+export const getHashParams = () => {
+    return window.location.hash
+        .substring(1)
+        .split('&')
+        .reduce(function (initial: { [key: string]: any; }, item) {
+            if (item) {
+                const parts = item.split('=')
+                initial[parts[0]] = decodeURIComponent(parts[1])
+            }
+
+            return initial
+        }, {})
+}
+
+export const removeHashParamsFromUrl = () => {
+    window.history.pushState('', document.title, window.location.pathname + window.location.search)
+}
