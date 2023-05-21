f862542e6f9c200c50d0cca4e0d2e851bafc13ac
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2023-05-21 09:29:12 +0200

    Create index.ts

---
+++ src/data/data_types/index.ts
@@ -0,0 +1,7 @@
+import {Track} from './track'
+import {Device} from './device'
+import {Album} from './album'
+import {TrendingTrack} from './trendingTrack'
+import {Artist} from './artist'
+
+export type {Track, Device, Album, TrendingTrack, Artist}
