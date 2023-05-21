2a0e0a6dd184f2772dbe6a302c49e0e5f9e911da
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2023-05-21 09:13:36 +0200

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {Route, Routes} from 'react-router'
+import LandingPage from './pages/LandingPage/LandingPage'
+import Overview from './pages/Overview'
+import Navbar from './components/ui/navbar/Navbar'
+import Sidebar from './components/ui/sidebar/Sidebar'
+import Action from './components/ui/action_section/Action'
+import PrivateRoute, {PrivateRouteProps} from './react_router_routes/PrivateRoute'
+import ProtectedRoute, {ProtectedRouteProps} from './react_router_routes/ProtectedRoute'
+import useUserSessionStore from './stores/user_session/userSessionStore'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const {accessToken} = useUserSessionStore()
+
+    const defaultPrivateRouteProps: Omit<PrivateRouteProps, 'outlet'> = {
+        isAuthenticated: accessToken !== undefined && accessToken !== '',
+        authenticationPath: '/',
+    }
+
+    const defaultProtectedRouteProps: Omit<ProtectedRouteProps, 'outlet'> = {
+        isAuthenticated: accessToken !== undefined && accessToken !== '',
+        redirectPath: '/dashboard/',
+    }
+
+    return (
+        <section id='main-section' style={{backgroundColor: 'var(--nl-clr-2)'}}>
+
+            {
+                accessToken !== undefined && accessToken !== '' ?
+                    <>
+                        <Navbar/>
+                        <Sidebar/>
+                        <Action/>
+                    </>
+                    : null
+            }
+
+            <Routes>
+                <Route path='/dashboard/'
+                       element={<PrivateRoute {...defaultPrivateRouteProps} outlet={<Overview/>}/>}/>
+                <Route path='*'
+                       element={<ProtectedRoute {...defaultProtectedRouteProps} outlet={<LandingPage/>}/>}/>
+            </Routes>
+        </section>
+    )
 }
 
-export default App;
+export default App
