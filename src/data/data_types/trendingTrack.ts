b69b213bedbc033eba5484aa22d116ded2a077b3
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2023-05-21 09:29:05 +0200

    Create trendingTrack.ts

---
+++ src/data/data_types/trendingTrack.ts
@@ -0,0 +1,8 @@
+export type TrendingTrack = {
+    items: [{
+        album: { images: [{ url: string }]; };
+        artists: [{ id: string; name: string; type: string; uri: string }];
+        name: string;
+        uri: string
+    }]
+}
