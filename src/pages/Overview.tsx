6a89eb665594981a5984d23b6bc9de8e1419d774
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2023-05-21 09:25:36 +0200

    Add trending section and data fetching

--- src/pages/Overview.tsx
+++ src/pages/Overview.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import Trending from '../components/ui/trending_section/Trending'
 
 const Overview = () => {
 
+    const getUserTrendings = useGetUserTrendingTracksQuery(1)
+    const [trendingTracks, setTrendingTracks] = useState<TrendingTrack>({
+        items: [{
+            album: {images: [{url: ''}]},
+            artists: [{id: '', name: '', type: '', uri: ''}],
+            name: '', uri: ''
+        }]
+    })
+    console.log(useUserSessionStore().accessToken)
+    useEffect(() => {
+        if (getUserTrendings.isSuccess) {
+            setTrendingTracks(getUserTrendings.data)
+        }
+    }, [getUserTrendings.data])
     return (
-        <div>
-            test
+        <div id='layout-container'>
+            <Trending trendingTracks={trendingTracks}/>
         </div>
     )
 }
