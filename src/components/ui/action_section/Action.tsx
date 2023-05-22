de9b6e00050bc56003482f84cb215e8f519bc52a
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2023-05-22 15:37:40 +0200

    Create Action.tsx

---
+++ src/components/ui/action_section/Action.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import FavArtists from '../fav_artists_section/FavArtists'
+import './Action.css'
+
+const Action = () => {
+
+    return (
+        <div id='action-container'>
+            <div>
+                <h1 className='fs-sc-1'>Shortcuts</h1>
+                <span>Comming soon</span>
+            </div>
+
+            <div className='action'>
+                <h1 className='fs-sc-1'>Fav Artists</h1>
+                <FavArtists/>
+            </div>
+        </div>
+    )
+}
+
+export default Action
