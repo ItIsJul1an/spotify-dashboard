86e47718fab074759ecf46f2e472f2d703618bcb
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2023-05-22 15:37:26 +0200

    Create Sidebar.tsx

---
+++ src/components/ui/sidebar/Sidebar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Sidebar.css'
+
+const Sidebar = () => {
+
+    return (
+        <aside id='sidebar-container'>
+            Sidebar
+        </aside>
+    )
+}
+
+export default Sidebar
