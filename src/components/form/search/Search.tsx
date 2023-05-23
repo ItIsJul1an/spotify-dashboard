b202f71b8bdf4335c5d36af1f75d98f61010e1cf
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2023-05-23 12:07:26 +0200

    Add SearchBar and recent searches displaying

--- src/components/form/search/Search.tsx
+++ src/components/form/search/Search.tsx
@@ -1,15 +1,23 @@
 import React, {useState} from 'react'
 import {createPortal} from 'react-dom'
 import SearchRoundedIcon from '@mui/icons-material/SearchRounded'
+import useOnClickOutside from '../../../hooks/useOnClickOutside'
+import SearchBar from '../search_bar/SearchBar'
+import SearchItemManager from '../../ui/search/SearchItemManager'
 import './Search.css'
-import useOnClickOutside from "../../../hooks/useOnClickOutside";
-import SearchBar from "../search_bar/SearchBar";
+import {Album} from "../../../data/data_types";
+import SearchItem from "../../ui/search/SearchItem";
+import useRecentSearchStore from "../../../stores/search/recentSearchStore";
 
 const Search = () => {
 
+    const {recentSearches, resetRecentSearch} = useRecentSearchStore()
+
     const [renderResult, setRender] = useState<boolean>(false)
+    const [searchResult, setSearchResult] = useState<any>(undefined)
 
     const [outClickRef] = useOnClickOutside(() => renderResult ? setRender(() => false) : null)
+    console.log(searchResult)
 
     return (
         <>
@@ -22,7 +30,24 @@ const Search = () => {
                 renderResult ? createPortal(
                     <div className='blocking-container'>
                         <div id='search-result-container' ref={outClickRef}>
-                            <SearchBar/>
+                            <SearchBar setSearchResult={setSearchResult}/>
+                            {
+                                searchResult ? <SearchItemManager data={searchResult}/> :
+                                    <div id='recent-search-container'>
+                                        <div className='stick-to-head'>
+                                            <h1>Recent</h1>
+                                            {
+                                                recentSearches.length !== 0 ? <button id='clear-recent'
+                                                                                      onClick={() => resetRecentSearch()}>Clear</button> : null
+                                            }
+                                        </div>
+                                        {
+                                            recentSearches.length !== 0 ? recentSearches.map((item: Album) => (
+                                                <SearchItem item={item}/>
+                                            )) : <span>No recent searches</span>
+                                        }
+                                    </div>
+                            }
                         </div>
                     </div>,
                     document.querySelector('#layout-container')!,
