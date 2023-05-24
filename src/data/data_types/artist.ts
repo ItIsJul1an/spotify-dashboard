63b899cd0a8b9d5174b09c9b5fdc763b9bb62b71
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2023-05-24 21:05:42 +0200

    Add images array

--- src/data/data_types/artist.ts
+++ src/data/data_types/artist.ts
@@ -2,7 +2,7 @@ export type Artist = {
     followers: number
     genres: string[]
     id: string
-    imageUrl: string
+    images: { height: number; width: number; url: string }[]
     name: string
     popularity: number
     type: string
