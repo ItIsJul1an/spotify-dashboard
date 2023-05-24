39229890fbd10bdf75fa13816b4bb1a9504da8eb
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2023-05-24 21:07:52 +0200

    Create FavArtistsPage.tsx

---
+++ src/pages/fav/artists/FavArtistsPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const FavArtistsPage = () => {
+
+    return (
+        <div id='layout-container'>
+            Fav Artists
+        </div>
+    )
+}
+
+export default FavArtistsPage
