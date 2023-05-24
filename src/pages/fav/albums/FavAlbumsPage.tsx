0fbff0f800eb8061c8cd3325d11840fb3e278139
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2023-05-24 21:07:55 +0200

    Create FavAlbumsPage.tsx

---
+++ src/pages/fav/albums/FavAlbumsPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const FavAlbumsPage = () => {
+
+    return (
+        <div id='layout-container'>
+            Fav Albums
+        </div>
+    )
+}
+
+export default FavAlbumsPage
