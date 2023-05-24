8c57e6b3f0964408cf3b8218de90d60568506388
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2023-05-24 21:08:52 +0200

    Add alternate image

--- src/components/ui/search/SearchItem.tsx
+++ src/components/ui/search/SearchItem.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Album, Artist, Track} from '../../../data/data_types'
+import spotify from '../../../data/images/spotify.svg'
 import './SearchItem.css'
 import useRecentSearchStore from "../../../stores/search/recentSearchStore";
 
@@ -19,13 +20,27 @@ const SearchItem = ({item}: SearchItemProps) => {
         <div id='search-item' onClick={onClickHandle}>
             <div>
                 {
-                    item.name
+                    (item as Album).uri.includes('album') ?
+                        (item as Album).images.length !== 0 ?
+                            <img src={(item as Album).images[0].url} alt='album image'/> :
+                            <img src={spotify} alt='spotify logo'/> :
+                        null
                 }
-                <span>
+                {
+                    (item as Artist).uri.includes('artist') ?
+                        (item as Artist).images.length !== 0 ?
+                            <img src={(item as Artist).images[0].url} alt='album image'/> :
+                            <img src={spotify} alt='spotify logo'/> :
+                        null
+                }
+                <div>
+                    {item.name}
+                    <span>
                     {
                         (item as Album).uri.includes('album') ? (item as Album).artists.map(artist => artist.name).join(', ') : null
                     }
                 </span>
+                </div>
             </div>
         </div>
     )
