82904785bf92eeb2ebf29033b779aae3a5e8abfd
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2023-05-24 21:08:01 +0200

    Create SidebarItemsManager.tsx

---
+++ src/components/ui/sidebar/SidebarItemsManager.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import {SidebarItems} from '../../../data/sidebar/sidebarItems'
+import SidebarListItem from './SidebarListItem'
+import './SidebarItemsManager.css'
+
+const SidebarItemsManager = () => {
+
+    return (
+        <ul id='sidebar-items-container'>
+            {
+                SidebarItems.map((group) => {
+                    return <div id='group-items-container'>
+                        <h1 className='fs-tr-body-1 fw--semi-bold nv-clr--heading'>{group.name}</h1>
+                        {
+                            group.items.map((item, index) => (
+                                <SidebarListItem key={`sidebar-item-${index}`}
+                                                 value={{title: item.title, image: item.image, url: item.url}}/>
+                            ))
+                        }
+                    </div>
+                })
+            }
+        </ul>
+    )
+}
+
+export default SidebarItemsManager
