0dac4de6fb56be30e47b660f3a1ba8d26b4ee244
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2023-05-24 21:07:49 +0200

    Create FavSongsPage.tsx

---
+++ src/pages/fav/songs/FavSongsPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const FavSongsPage = () => {
+
+    return (
+        <div id='layout-container'>
+            Fav Songs
+        </div>
+    )
+}
+
+export default FavSongsPage
