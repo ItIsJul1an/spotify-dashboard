f80589e9ec82992d63d43e75da7c9d89c7648873
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2023-05-27 19:51:40 +0200

    Add no data displaying

--- src/components/ui/trending_section/Trending.tsx
+++ src/components/ui/trending_section/Trending.tsx
@@ -15,21 +15,27 @@ const Trending = ({trendingTracks}: TrendingProps) => {
             <div>
                 <h1 className='fs-pr-1 fw'>Trending</h1>
             </div>
+
             <div id='trending-player-container'
+                 className={trendingTracks.items[0].uri === '' ? 'inactive-trending' : undefined}
                  style={{background: `linear-gradient(45deg, hsl(0, 0%, 100%) 30%, hsl(0, 0%, 100%, 20%)), url(${trendingTracks.items[0].album.images[0].url})`}}>
-                <span className='fw--semi-bold'>{trendingTracks.items[0].artists[0].name}</span>
-                <h1 className='fs-sc-1'>
-                    {trendingTracks.items[0].name}
-                </h1>
-                <div>
-                    <PlayButton trackUri={trendingTracks.items[0].uri}
-                                style={{backgroundColor: 'hsl(0, 0%, 0%)', color: 'hsl(0, 0%, 100%)'}}/>
-                    <FollowButton artistUri={trendingTracks.items[0].artists[0].id} style={{
-                        backgroundColor: 'transparent',
-                        color: 'hsl(0, 0%, 0%)',
-                        border: '1px solid hsl(0, 0%, 0%)'
-                    }}/>
-                </div>
+                {
+                    trendingTracks.items[0].uri !== '' ?
+                        <>
+                            <span className='fw--semi-bold'>{trendingTracks.items[0].artists[0].name}</span>
+                            <h1 className='fs-sc-1'>{trendingTracks.items[0].name}</h1>
+                            <div>
+                                <PlayButton trackUri={trendingTracks.items[0].uri}
+                                            style={{backgroundColor: 'hsl(0, 0%, 0%)', color: 'hsl(0, 0%, 100%)'}}/>
+                                <FollowButton artistUri={trendingTracks.items[0].artists[0].id} style={{
+                                    backgroundColor: 'transparent',
+                                    color: 'hsl(0, 0%, 0%)',
+                                    border: '1px solid hsl(0, 0%, 0%)'
+                                }}/>
+                            </div>
+                        </>
+                        : <h1 className='fs-sc-1'>Nothing to display</h1>
+                }
             </div>
         </section>
     )
