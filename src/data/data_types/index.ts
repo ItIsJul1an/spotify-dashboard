22f6f119e19f832b94be4e74fe2aa1b4a6764642
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2023-06-02 20:54:51 +0200

    Add playlist type

--- src/data/data_types/index.ts
+++ src/data/data_types/index.ts
@@ -4,5 +4,6 @@ import {Album} from './album'
 import {TrendingTrack} from './trendingTrack'
 import {Artist} from './artist'
 import {User} from './user'
+import {Playlist} from './playlist'
 
-export type {Track, Device, Album, TrendingTrack, Artist, User}
+export type {Track, Device, Album, TrendingTrack, Artist, User, Playlist}
