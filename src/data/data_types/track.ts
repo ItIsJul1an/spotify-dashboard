9b92332e5314ff1d9c8b1cbf58a76bf9d2d69f76
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2023-06-02 20:54:33 +0200

    Add timestamp and artist name

--- src/data/data_types/track.ts
+++ src/data/data_types/track.ts
@@ -5,6 +5,7 @@ export type Track = {
     popularity: number
     duration_ms: number
     is_playing: boolean
+    timestamp: number
     playing_progress_ms?: number
     type: string
     uri: string
@@ -16,5 +17,6 @@ export type Track = {
     artists: {
         id: string
         uri: string
+        name: string
     }[]
 }
