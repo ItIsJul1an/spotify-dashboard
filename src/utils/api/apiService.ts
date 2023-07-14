b999f35ae01590cb1d0102897875ddd7a622c673
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2023-07-14 17:40:16 +0200

    Update apiService.ts

--- src/utils/api/apiService.ts
+++ src/utils/api/apiService.ts
@@ -4,6 +4,20 @@ import {toast} from 'react-toastify'
 import {webApiEndpoint} from '../../data/configs/webApi/webApiConfig'
 import useUserSessionStore from '../../stores/user_session/userSessionStore'
 import useTracksStore from '../../stores/tracks/useTrackStore'
+import useUserStore from "../../stores/users/useUserStore";
+
+export const useGetMyProfileQuery = () => {
+    const {accessToken} = useUserSessionStore()
+
+    return useQuery(['myProfile'], () =>
+            axios.get(`${webApiEndpoint}/me`, {
+                headers: {Authorization: `Bearer ${accessToken}`}
+            }).then((res) => res.data), {
+            enabled: accessToken !== undefined && accessToken !== '',
+            onError: (err) => toast.error('Something went wrong: ' + (err as AxiosError).message)
+        }
+    )
+}
 
 export const useGetUserTrendingTracksQuery = (limit: number = 20) => {
     const {accessToken} = useUserSessionStore()
@@ -95,6 +109,46 @@ export const useGetCheckFollowUserQuery = (user: string) => {
     )
 }
 
+export const useGetCheckFollowPlaylistQuery = (playlistId: string | undefined, userId?: string) => {
+    const {accessToken} = useUserSessionStore()
+    const {myProfile} = useUserStore()
+
+    return useQuery(['checkFollowPlaylist', playlistId], () =>
+            axios.get(`${webApiEndpoint}/playlists/${playlistId}/followers/contains?ids=${userId ? userId : myProfile.id}`, {
+                headers: {Authorization: `Bearer ${accessToken}`}
+            }).then((res) => res.data), {
+            enabled: accessToken !== undefined && accessToken !== '' && playlistId !== undefined,
+            onError: (err) => toast.error('Something went wrong: ' + (err as AxiosError).message)
+        }
+    )
+}
+
+export const useGetCheckFollowAlbumQuery = (albumId: string | undefined) => {
+    const {accessToken} = useUserSessionStore()
+
+    return useQuery(['checkFollowAlbum', albumId], () =>
+            axios.get(`${webApiEndpoint}/me/albums/contains?ids=${albumId}`, {
+                headers: {Authorization: `Bearer ${accessToken}`}
+            }).then((res) => res.data), {
+            enabled: accessToken !== undefined && accessToken !== '' && albumId !== undefined,
+            onError: (err) => toast.error('Something went wrong: ' + (err as AxiosError).message)
+        }
+    )
+}
+
+export const useGetCheckFollowTrackQuery = (trackId: string | undefined) => {
+    const {accessToken} = useUserSessionStore()
+
+    return useQuery(['checkFollowTrack', trackId], () =>
+            axios.get(`${webApiEndpoint}/me/tracks/contains?ids=${trackId}`, {
+                headers: {Authorization: `Bearer ${accessToken}`}
+            }).then((res) => res.data), {
+            enabled: accessToken !== undefined && accessToken !== '' && trackId !== undefined,
+            onError: (err) => toast.error('Something went wrong: ' + (err as AxiosError).message)
+        }
+    )
+}
+
 export const useGetSearchQuery = (searchQuery: string = '') => {
     const {accessToken} = useUserSessionStore()
 
@@ -118,6 +172,18 @@ export const useGetCurrentlyPlayingQuery = () => {
         }).then((res) => res.data), {
         enabled: accessToken !== undefined && accessToken !== '',
         refetchInterval: playingTrack && playingTrack.playing_progress_ms ? playingTrack.duration_ms - playingTrack.playing_progress_ms + 400 : false,
+        onError: (err) => toast.error('Something went wrong: ' + (err as AxiosError).message)
+    })
+}
+
+export const useGetPlaylistItemsQuery = (playlistId: string | undefined) => {
+    const {accessToken} = useUserSessionStore()
+
+    return useQuery(['playlistItems'], () =>
+        axios.get(`${webApiEndpoint}/playlists/${playlistId}/tracks`, {
+            headers: {Authorization: `Bearer ${accessToken}`}
+        }).then((res) => res.data), {
+        enabled: accessToken !== undefined && accessToken !== '' && playlistId !== undefined,
         onError: (err) => toast.error('Something went wrong: ' + (err as AxiosError).message)
     })
 }
@@ -149,7 +215,7 @@ export const usePlayTrackMutation = (device?: string) => {
         onSuccess: () => {
             setTimeout(() => {
                 queryClient.invalidateQueries(['currentlyPlayingTrack'])
-            }, 400)
+            }, 800)
         },
         onError: (err) => toast.error('Cannot play track: ' + (err as AxiosError).message)
     })
@@ -169,7 +235,7 @@ export const usePauseTrackMutation = (device?: string) => {
         onSuccess: () => {
             setTimeout(() => {
                 queryClient.invalidateQueries(['currentlyPlayingTrack'])
-            }, 400)
+            }, 800)
         },
         onError: (err) => toast.error('Cannot pause track: ' + (err as AxiosError).message)
     })
@@ -208,5 +274,77 @@ export const useUnfollowArtistMutation = () => {
             }, 400)
         },
         onError: (err) => toast.error('Cannot unfollow artist: ' + (err as AxiosError).message)
+    })
+}
+
+export const useSaveAlbumMutation = () => {
+    const {accessToken} = useUserSessionStore()
+    const queryClient = useQueryClient()
+
+    return useMutation((albumId: string) => {
+        return axios.put(`${webApiEndpoint}/me/albums?ids=${albumId}`, null, {
+            headers: {Authorization: `Bearer ${accessToken}`}
+        })
+    }, {
+        onSuccess: () => {
+            setTimeout(() => {
+                //queryClient.invalidateQueries(['followedArtists'])
+            }, 400)
+        },
+        onError: (err) => toast.error('Cannot save album: ' + (err as AxiosError).message)
+    })
+}
+
+export const useRemoveAlbumMutation = () => {
+    const {accessToken} = useUserSessionStore()
+    const queryClient = useQueryClient()
+
+    return useMutation((albumId: string) => {
+        return axios.delete(`${webApiEndpoint}/me/albums?ids=${albumId}`, {
+            headers: {Authorization: `Bearer ${accessToken}`}
+        })
+    }, {
+        onSuccess: () => {
+            setTimeout(() => {
+                //queryClient.invalidateQueries(['followedArtists'])
+            }, 400)
+        },
+        onError: (err) => toast.error('Cannot remove album: ' + (err as AxiosError).message)
+    })
+}
+
+export const useSaveTrackMutation = () => {
+    const {accessToken} = useUserSessionStore()
+    const queryClient = useQueryClient()
+
+    return useMutation((albumId: string) => {
+        return axios.put(`${webApiEndpoint}/me/tracks?ids=${albumId}`, null, {
+            headers: {Authorization: `Bearer ${accessToken}`}
+        })
+    }, {
+        onSuccess: () => {
+            setTimeout(() => {
+                //queryClient.invalidateQueries(['followedArtists'])
+            }, 400)
+        },
+        onError: (err) => toast.error('Cannot save track: ' + (err as AxiosError).message)
+    })
+}
+
+export const useRemoveTrackMutation = () => {
+    const {accessToken} = useUserSessionStore()
+    const queryClient = useQueryClient()
+
+    return useMutation((albumId: string) => {
+        return axios.delete(`${webApiEndpoint}/me/tracks?ids=${albumId}`, {
+            headers: {Authorization: `Bearer ${accessToken}`}
+        })
+    }, {
+        onSuccess: () => {
+            setTimeout(() => {
+                //queryClient.invalidateQueries(['followedArtists'])
+            }, 400)
+        },
+        onError: (err) => toast.error('Cannot remove track: ' + (err as AxiosError).message)
     })
 }
