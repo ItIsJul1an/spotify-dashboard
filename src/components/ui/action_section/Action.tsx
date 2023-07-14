226aa96a097457ed3bc69d785d8267644de6eeb9
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2023-07-14 17:40:01 +0200

    Remove Shortcuts

--- src/components/ui/action_section/Action.tsx
+++ src/components/ui/action_section/Action.tsx
@@ -7,11 +7,6 @@ const Action = () => {
 
     return (
         <div id='action-container'>
-            <div>
-                <h1 className='fs-sc-1'>Shortcuts</h1>
-                <span>Comming soon</span>
-            </div>
-
             <div className='action'>
                 <h1 className='fs-sc-1'>Fav Artists</h1>
                 <FavArtists/>
