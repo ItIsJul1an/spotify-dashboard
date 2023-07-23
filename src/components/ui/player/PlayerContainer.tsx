433443895c0aa8502f1f28b367a5775a3245a476
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2023-07-23 13:12:01 +0200

    Update PlayerContainer.tsx

--- src/components/ui/player/PlayerContainer.tsx
+++ src/components/ui/player/PlayerContainer.tsx
@@ -1,12 +1,36 @@
 import React from 'react'
 import './PlayerContainer.css'
+import ProgressSlider from '../../form/progress_slider/ProgressSlider'
+import PlayButton from '../../form/buttons/filled/playButton/PlayButton'
+import useTracksStore from '../../../stores/tracks/useTrackStore'
+import PrevButton from "../../form/buttons/default/skipButton/prevButton/PrevButton";
+import NextButton from "../../form/buttons/default/skipButton/nextButton/NextButton";
 
 const PlayerContainer = () => {
 
-    return (
-        <div>
+    const {playingTrack} = useTracksStore()
 
-        </div>
+    const onClickHandle = () => {
+
+    }
+
+    return (
+        playingTrack ?
+            <div id='player-container'>
+                <div id='controls'>
+                    <PrevButton/>
+                    <PlayButton id='player-container--play' trackUri={playingTrack.uri}
+                                itemType={playingTrack.type}
+                                displayContent='image'
+                                onClickHandle={onClickHandle}/>
+                    <NextButton/>
+                </div>
+
+                <div>
+                    <ProgressSlider/>
+                </div>
+            </div>
+            : null
     )
 }
 
