e29e894112f29a4af88179dc718ace611f5a186e
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2023-07-23 13:11:49 +0200

    Update FollowButton.tsx

--- src/components/form/buttons/outlined/followButton/FollowButton.tsx
+++ src/components/form/buttons/outlined/followButton/FollowButton.tsx
@@ -1,21 +1,31 @@
-import React, {ButtonHTMLAttributes, useEffect, useState} from 'react'
+import React, {ButtonHTMLAttributes, createElement, useEffect, useState} from 'react'
 import './FollowButton.css'
 import {
     useFollowArtistMutation,
     useGetCheckFollowArtistQuery,
     useUnfollowArtistMutation
 } from '../../../../../utils/api/apiService'
+import FavoriteRoundedIcon from '@mui/icons-material/FavoriteRounded'
+import HeartBrokenRoundedIcon from '@mui/icons-material/HeartBrokenRounded'
+import useHover from "../../../../../hooks/useHover";
+import TooltipManager from "../../../../ui/tooltip/TooltipManager";
 
 interface FollowButtonProps {
     artistUri: string
+    displayContent?: 'text' | 'image'
+    tooltip?: string
     onClickHandle?: Function
 }
 
 const FollowButton = ({
                           artistUri,
+                          displayContent = 'text',
+                          tooltip,
                           onClickHandle,
                           ...props
                       }: FollowButtonProps & ButtonHTMLAttributes<HTMLButtonElement>) => {
+
+    const [hoverRef, isHovered] = useHover()
 
     const [isFollowing, setFollowing] = useState<boolean>(false)
 
@@ -43,15 +53,27 @@ const FollowButton = ({
     }, [unfollowArtist.data])
 
     return (
-        <button id='follow-button' aria-label='Follow button' {...props}
-                onClick={() => {
-                    onClickHandle?.()
-                    if (artistUri !== '') {
-                        isFollowing ? unfollowArtist.mutate(artistUri) : followArtist.mutate(artistUri)
-                    }
-                }}>
-            {isFollowing ? 'UNFOLLOW' : 'FOLLOW'}
-        </button>
+        <>
+            <button id='follow-button' ref={hoverRef} aria-label='Follow button' {...props}
+                    onClick={() => {
+                        onClickHandle?.()
+                        if (artistUri !== '' && artistUri !== undefined) {
+                            isFollowing ? unfollowArtist.mutate(artistUri) : followArtist.mutate(artistUri)
+                        }
+                    }}>
+                {displayContent === 'image' ? isFollowing ? createElement<any>(HeartBrokenRoundedIcon, {style: {fontSize: '24px'}})
+                        : createElement<any>(FavoriteRoundedIcon, {style: {fontSize: '24px'}}) :
+                    displayContent === 'text' ? isFollowing ? 'UNFOLLOW' : 'FOLLOW' : null}
+            </button>
+
+            {
+                (isHovered) ? <TooltipManager content={
+                    <span className='fs-sc-body-1 fw--semi-bold'>
+                        {tooltip ? tooltip : isFollowing ? 'Unfollow artist' : 'Follow artist'}
+                    </span>
+                }/> : null
+            }
+        </>
     )
 }
 
