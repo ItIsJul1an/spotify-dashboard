50b15b298ab24e70837b55c31cadbbe5c1aed654
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2023-07-23 13:12:06 +0200

    Update SearchItemManager.tsx

--- src/components/ui/search/SearchItemManager.tsx
+++ src/components/ui/search/SearchItemManager.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Album, Artist, Track} from '../../../data/data_types'
+import {Album, Artist, Playlist, Track} from '../../../data/data_types'
 import SearchItem from './SearchItem'
 import './SearchItemManager.css'
 import Accordion from "../accordion/Accordion";
@@ -37,6 +37,16 @@ const SearchItemManager = ({data}: SearchItemManagerProps) => {
                     {
                         data.tracks.items.map((item: Track, index: number) => (
                             <SearchItem key={`track-search-item-${index}`} item={item}/>
+                        ))
+                    }
+                </>}/>
+            </div>
+
+            <div>
+                <Accordion header='Playlists' content={<>
+                    {
+                        data.playlists.items.map((item: Playlist, index: number) => (
+                            <SearchItem key={`playlist-search-item-${index}`} item={item}/>
                         ))
                     }
                 </>}/>
