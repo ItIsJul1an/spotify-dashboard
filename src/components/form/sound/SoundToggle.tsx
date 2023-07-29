0a94ea0e751e99025160f84499665633c50dea2e
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2023-07-29 16:27:29 +0200

    Create SoundToggle.tsx

---
+++ src/components/form/sound/SoundToggle.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './SoundToggle.css'
+
+const SoundToggle = () => {
+
+    return (
+        <div>
+            Toggle
+        </div>
+    )
+}
+
+export default SoundToggle
