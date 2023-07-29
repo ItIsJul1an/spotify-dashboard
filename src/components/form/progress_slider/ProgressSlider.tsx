8c69ddaae326bf3cf800adff09f30ef7a81a6f06
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2023-07-29 16:27:33 +0200

    Update ProgressSlider.tsx

--- src/components/form/progress_slider/ProgressSlider.tsx
+++ src/components/form/progress_slider/ProgressSlider.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent} from 'react'
+import React, {ChangeEvent, memo} from 'react'
 import {toNumber} from 'lodash'
 import {Duration, intervalToDuration} from 'date-fns'
 import useTracksStore from '../../../stores/tracks/useTrackStore'
@@ -7,38 +7,38 @@ import './ProgressSlider.css'
 
 const ProgressSlider = () => {
 
-const {playingTrack} = useTracksStore()
+    const {playingTrack} = useTracksStore()
 
-const skipToPositionMutation = useSkipToPositionMutation()
+    const skipToPositionMutation = useSkipToPositionMutation()
 
-const progress: Duration | undefined = playingTrack && playingTrack.playing_progress_ms ? intervalToDuration({
-    start: 0,
-    end: playingTrack.playing_progress_ms
-}) : undefined
+    const progress: Duration | undefined = playingTrack && playingTrack.playing_progress_ms ? intervalToDuration({
+        start: 0,
+        end: playingTrack.playing_progress_ms
+    }) : undefined
 
-const duration: Duration | undefined = playingTrack ? intervalToDuration({
-    start: 0,
-    end: playingTrack.duration_ms
-}) : undefined
+    const duration: Duration | undefined = playingTrack ? intervalToDuration({
+        start: 0,
+        end: playingTrack.duration_ms
+    }) : undefined
 
-const onProgressChange = (event: ChangeEvent<HTMLInputElement>) => {
-    skipToPositionMutation.mutate(toNumber(event.target.value))
+    const onProgressChange = (event: ChangeEvent<HTMLInputElement>) => {
+        skipToPositionMutation.mutate(toNumber(event.target.value))
+    }
+    
+    return (
+        playingTrack && progress && duration ?
+            <div id='progress-container'>
+                <span>{progress?.minutes}:{progress.seconds! < 10 ? '0' + progress.seconds! : progress?.seconds}</span>
+
+                <input id='progress-indicator' type='range'
+                       value={playingTrack.playing_progress_ms}
+                       max={playingTrack.duration_ms}
+                       onChange={onProgressChange}/>
+
+                <span>{duration?.minutes}:{duration.seconds! < 10 ? '0' + duration.seconds! : duration?.seconds}</span>
+            </div>
+            : null
+    )
 }
 
-return (
-    playingTrack && progress && duration ?
-        <div id='progress-container'>
-            <span>{progress?.minutes}:{progress.seconds! < 10 ? '0' + progress.seconds! : progress?.seconds}</span>
-
-            <input id='progress-indicator' type='range'
-                   value={playingTrack.playing_progress_ms}
-                   max={playingTrack.duration_ms}
-                   onChange={onProgressChange}/>
-
-            <span>{duration?.minutes}:{duration.seconds! < 10 ? '0' + duration.seconds! : duration?.seconds}</span>
-        </div>
-        : null
-)
-}
-
-export default ProgressSlider
+export default memo(ProgressSlider)
