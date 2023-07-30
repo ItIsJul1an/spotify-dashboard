3b2a898ee700a529d4876dea9a390ec9d93f61b6
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2023-07-30 13:46:32 +0200

    Update SoundToggle.tsx

--- src/components/form/sound/SoundToggle.tsx
+++ src/components/form/sound/SoundToggle.tsx
@@ -1,12 +1,49 @@
-import React from 'react'
+import React, {ChangeEvent} from 'react'
+import VolumeUpRoundedIcon from '@mui/icons-material/VolumeUpRounded'
+import VolumeOffRoundedIcon from '@mui/icons-material/VolumeOffRounded'
+import VolumeMuteRoundedIcon from '@mui/icons-material/VolumeMuteRounded'
 import './SoundToggle.css'
+import useHover from "../../../hooks/useHover";
+import TooltipManager from "../../ui/tooltip/TooltipManager";
 
 const SoundToggle = () => {
 
+    const [volumeMuteRef, isMuteHovered] = useHover()
+    const [volumeUpRef, isUpHovered] = useHover()
+
+    const onProgressChange = (event: ChangeEvent<HTMLInputElement>) => {
+
+    }
+
     return (
-        <div>
-            Toggle
-        </div>
+        <>
+            <div id='volume-container'>
+                <div className='control' ref={volumeMuteRef}>
+                    <VolumeMuteRoundedIcon/>
+                </div>
+
+                <input id='volume-indicator' type='range'
+                       value={50}
+                       max={100}
+                       onChange={onProgressChange}/>
+
+                <div className='control' ref={volumeUpRef}>
+                    <VolumeUpRoundedIcon/>
+                </div>
+            </div>
+
+            {
+                (isMuteHovered) ? <TooltipManager content={
+                    <span className='fs-sc-body-1 fw--semi-bold'>Volume Down</span>
+                }/> : null
+            }
+
+            {
+                (isUpHovered) ? <TooltipManager content={
+                    <span className='fs-sc-body-1 fw--semi-bold'>Volume Up</span>
+                }/> : null
+            }
+        </>
     )
 }
 
