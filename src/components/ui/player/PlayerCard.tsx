035cbd65fb4e420ed4d11a1780c193ebfdeef7ab
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2023-07-31 12:39:08 +0200

    Fix alternate text

--- src/components/ui/player/PlayerCard.tsx
+++ src/components/ui/player/PlayerCard.tsx
@@ -39,7 +39,7 @@ const PlayerCard = () => {
                         </div>
                     </>
                     :
-                    <h1>Currently not playing</h1>
+                    <h1>Currently nothing playing</h1>
             }
 
             {
