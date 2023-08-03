89a38eb4cee8f2ce29ebd8f70ac0d795d52f6993
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2023-08-03 13:51:39 +0200

    Create ShuffleButton.tsx

---
+++ src/components/form/buttons/default/shuffle_button/ShuffleButton.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import ShuffleRoundedIcon from '@mui/icons-material/ShuffleRounded'
+import useHover from '../../../../../hooks/useHover'
+import usePlaybackStore from '../../../../../stores/playback_state/usePlaybackStore'
+import {useSetTogglePlaybackShuffleMutation} from '../../../../../utils/api/apiService'
+import TooltipManager from '../../../../ui/tooltip/TooltipManager'
+import './ShuffleButton.css'
+
+const ShuffleButton = () => {
+
+    const {playback} = usePlaybackStore()
+
+    const [hoverRef, isHovered] = useHover()
+
+    const setShuffleModeMutation = useSetTogglePlaybackShuffleMutation()
+
+    return (
+        <>
+            <div id='shuffle-container' ref={hoverRef} onClick={() => {
+                setShuffleModeMutation.mutate(!playback.shuffle_state)
+            }}>
+                <ShuffleRoundedIcon fontSize='medium'/>
+            </div>
+
+            {
+                (isHovered) ? <TooltipManager content={
+                    <span className='fs-sc-body-1 fw--semi-bold'>{playback.shuffle_state ? 'Deactivate' : 'Activate'} shuffle</span>
+                }/> : null
+            }
+        </>
+    )
+}
+
+export default ShuffleButton
