2ccb66157f293e2161f95a8831d91606b137bb76
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2023-08-03 13:52:40 +0200

    Create playback.ts

---
+++ src/data/data_types/playback.ts
@@ -0,0 +1,142 @@
+export type Playback = {
+    device: {
+        id: string | undefined
+        is_active: boolean
+        is_private_session: boolean
+        is_restricted: boolean
+        name: string
+        type: string
+        volume_percent: number
+    }
+    repeat_state: string
+    shuffle_state: boolean
+    context: {
+        type: string
+        href: string
+        external_urls: {
+            spotify: string
+        }
+        uri: string
+    }
+    timestamp: number
+    progress_ms: number
+    is_playing: boolean
+    /*item: {
+        album: {
+            album_type: string
+            total_tracks: number
+            available_markets: string[]
+            external_urls: {
+                spotify: string
+            }
+            href: string
+            id: string
+            images: [
+                {
+                    url: string
+                    height: number
+                    width: number
+                }
+            ]
+            name: string
+            release_date: Date
+            release_date_precision: string
+            restrictions: {
+                reason: string
+            }
+            type: string
+            uri: string
+            copyrights: [
+                {
+                    text: string
+                    type: string
+                }
+            ]
+            external_ids: {
+                isrc: string
+                ean: string
+                upc: string
+            }
+            genres: string[]
+            label: string
+            popularity: number
+            album_group: string
+            artists: [
+                {
+                    external_urls: {
+                        spotify: string
+                    }
+                    href: string
+                    id: string
+                    name: string
+                    type: string
+                    uri: string
+                }
+            ]
+        }
+        artists: [
+            {
+                external_urls: {
+                    spotify: string
+                }
+                followers: {
+                    href: string
+                    total: number
+                }
+                genres: string[]
+                href: string
+                id: string
+                images: [
+                    {
+                        url: string
+                        height: number
+                        width: number
+                    }
+                ]
+                name: string
+                popularity: number
+                type: string
+                uri: string
+            }
+        ]
+        available_markets: string[]
+        disc_number: number
+        duration_ms: number
+        explicit: boolean
+        external_ids: {
+            isrc: string
+            ean: string
+            upc: string
+        }
+        external_urls: {
+            spotify: string
+        }
+        href: string
+        id: string
+        is_playable: boolean
+        linked_from: {}
+        restrictions: {
+            reason: string
+        }
+        name: string
+        popularity: number
+        preview_url: string
+        track_number: number
+        type: string
+        uri: string
+        is_local: boolean
+    }
+    currently_playing_type: string
+    actions: {
+        interrupting_playback: boolean
+        pausing: boolean
+        resuming: boolean
+        seeking: boolean
+        skipping_next: boolean
+        skipping_prev: boolean
+        toggling_repeat_context: boolean
+        toggling_shuffle: boolean
+        toggling_repeat_track: boolean
+        transferring_playback: boolean
+    }*/
+}
