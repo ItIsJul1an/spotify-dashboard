1533ef32076e6aa4457391e703aa244b99888005
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2023-08-21 16:45:17 +0200

    Update RepeatButton.tsx

--- src/components/form/buttons/default/repeat_button/RepeatButton.tsx
+++ src/components/form/buttons/default/repeat_button/RepeatButton.tsx
@@ -2,10 +2,13 @@ import React from 'react'
 import RepeatRoundedIcon from '@mui/icons-material/RepeatRounded'
 import useHover from '../../../../../hooks/useHover'
 import {useSetRepeatModeMutation} from '../../../../../utils/api/apiService'
+import usePlaybackStore from '../../../../../stores/playback_state/usePlaybackStore'
 import TooltipManager from '../../../../ui/tooltip/TooltipManager'
 import './RepeatButton.css'
 
 const RepeatButton = () => {
+
+    const {playback} = usePlaybackStore()
 
     const [hoverRef, isHovered] = useHover()
 
@@ -13,13 +16,21 @@ const RepeatButton = () => {
 
     return (
         <>
-            <div id='repeat-container' ref={hoverRef} onClick={() => setRepeatModeMutation.mutate('track')}>
-                <RepeatRoundedIcon fontSize='medium'/>
+            <div id='repeat-container' ref={hoverRef} onClick={() => {
+                if (playback.repeat_state === 'track') {
+                    setRepeatModeMutation.mutate('off')
+                } else {
+                    setRepeatModeMutation.mutate('track')
+                }
+            }}>
+                <RepeatRoundedIcon fontSize='large'/>
             </div>
 
             {
                 (isHovered) ? <TooltipManager content={
-                    <span className='fs-sc-body-1 fw--semi-bold'>Activate repeat</span>
+                    <span className='fs-sc-body-1 fw--semi-bold'>
+                        {playback.repeat_state === 'track' ? 'Deactivate' : 'Activate'} repeat
+                    </span>
                 }/> : null
             }
         </>
