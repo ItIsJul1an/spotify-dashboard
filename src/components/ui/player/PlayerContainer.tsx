158bf6d3da7db58f104f072e03c8b6f6db0f02b3
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2023-08-21 16:45:30 +0200

    Update PlayerContainer.tsx

--- src/components/ui/player/PlayerContainer.tsx
+++ src/components/ui/player/PlayerContainer.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect, useRef} from 'react'
 import useTracksStore from '../../../stores/tracks/useTrackStore'
 import ProgressSlider from '../../form/progress_slider/ProgressSlider'
 import PlayButton from '../../form/buttons/filled/playButton/PlayButton'
@@ -6,42 +6,65 @@ import PrevButton from '../../form/buttons/default/skipButton/prevButton/PrevBut
 import NextButton from '../../form/buttons/default/skipButton/nextButton/NextButton'
 import FavouriteButton from '../../form/buttons/outlined/favouriteButton/FavouriteButton'
 import SoundToggle from '../../form/sound/SoundToggle'
-import './PlayerContainer.css'
 import RepeatButton from "../../form/buttons/default/repeat_button/RepeatButton";
 import ShuffleButton from "../../form/buttons/default/shuffle_button/ShuffleButton";
+import FullSizeButton from "../../form/buttons/outlined/full_size/FullSizeButton";
+import './PlayerContainer.css'
+import useWebsiteStateStore from "../../../stores/website/useWebsiteStateStore";
 
 const PlayerContainer = () => {
 
+    const playContainerRef = useRef<HTMLDivElement>(null)
+
     const {playingTrack} = useTracksStore()
+    const {websiteState} = useWebsiteStateStore()
+
+    useEffect(() => {
+        if (playContainerRef.current === null)
+            return
+
+        const refClassList: DOMTokenList = playContainerRef.current.classList
+
+        if (websiteState.playerFullSize) {
+            refClassList.add('active--full-size--ref-container')
+            playContainerRef.current.style.height = '100vh'
+            playContainerRef.current.style.backgroundImage = `url(${playingTrack!.album.images[0].url})`
+        } else {
+            refClassList.remove('active--full-size--ref-container')
+            playContainerRef.current.style.height = '120px'
+            playContainerRef.current.style.backgroundImage = 'unset'
+        }
+    }, [websiteState.playerFullSize])
 
     return (
         playingTrack ?
-            <div id='player-container'>
-                <div className='controls' style={{justifySelf: 'flex-start', gridArea: 'left'}}>
-                    <FavouriteButton id='player-container--favourite' itemId={playingTrack.id}
-                                     itemType={playingTrack.type} displayContent='image'/>
-                    <FavouriteButton id='player-container--favourite' itemId={playingTrack.id}
-                                     itemType={playingTrack.type} displayContent='image'/>
-                    <FavouriteButton id='player-container--favourite' itemId={playingTrack.id}
-                                     itemType={playingTrack.type} displayContent='image'/>
-                </div>
+            <div ref={playContainerRef}>
+                <div id='player-container'>
+                    <div className='controls' style={{justifySelf: 'flex-start', gridArea: 'left'}}>
+                        <FavouriteButton id='player-container--favourite' itemId={playingTrack.id}
+                                         itemType={playingTrack.type} displayContent='image'/>
+                        <FullSizeButton/>
+                        <FavouriteButton id='player-container--favourite' itemId={playingTrack.id}
+                                         itemType={playingTrack.type} displayContent='image'/>
+                    </div>
 
-                <div className='controls' style={{gridArea: 'middle'}}>
-                    <RepeatButton/>
-                    <PrevButton/>
-                    <PlayButton id='player-container--play' trackUri={playingTrack.uri}
-                                itemType={playingTrack.type}
-                                displayContent='image'/>
-                    <NextButton/>
-                    <ShuffleButton/>
-                </div>
+                    <div className='controls' style={{gridArea: 'middle'}}>
+                        <RepeatButton/>
+                        <PrevButton/>
+                        <PlayButton id='player-container--play' trackUri={playingTrack.uri}
+                                    itemType={playingTrack.type}
+                                    displayContent='image'/>
+                        <NextButton/>
+                        <ShuffleButton/>
+                    </div>
 
-                <div className='controls' style={{justifySelf: 'flex-end', gridArea: 'right'}}>
-                    <SoundToggle/>
-                </div>
+                    <div className='controls' style={{justifySelf: 'flex-end', gridArea: 'right'}}>
+                        <SoundToggle/>
+                    </div>
 
-                <div style={{alignSelf: 'center', gridArea: 'down'}}>
-                    <ProgressSlider/>
+                    <div style={{alignSelf: 'center', gridArea: 'down'}}>
+                        <ProgressSlider/>
+                    </div>
                 </div>
             </div>
             : null
