4220fc41e9dc89b1dbf6fe5d579460fcc771651d
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2023-08-24 16:42:59 +0200

    Add full-size features

--- src/components/ui/player/PlayerContainer.tsx
+++ src/components/ui/player/PlayerContainer.tsx
@@ -11,41 +11,77 @@ import ShuffleButton from "../../form/buttons/default/shuffle_button/ShuffleButt
 import FullSizeButton from "../../form/buttons/outlined/full_size/FullSizeButton";
 import './PlayerContainer.css'
 import useWebsiteStateStore from "../../../stores/website/useWebsiteStateStore";
+import useMouseMove from "../../../hooks/useMouseMove";
 
 const PlayerContainer = () => {
 
-    const playContainerRef = useRef<HTMLDivElement>(null)
+    const playerParentRef = useRef<HTMLDivElement>(null)
+    const playerContainerRef = useRef<HTMLDivElement>(null)
+    const fullSizeRef = useRef<HTMLDivElement>(null)
+    const fullSizeImgRef = useRef<HTMLImageElement>(null)
+
+    const isMouseMoving = useMouseMove(2_000)
 
     const {playingTrack} = useTracksStore()
     const {websiteState} = useWebsiteStateStore()
 
+    const artists = playingTrack ? playingTrack.artists.map(artist => artist.name).join(', ') : undefined
+
     useEffect(() => {
-        if (playContainerRef.current === null)
+        if (playerParentRef.current === null || playerContainerRef.current === null)
             return
 
-        const refClassList: DOMTokenList = playContainerRef.current.classList
+        const refClassList: DOMTokenList = playerParentRef.current.classList
 
         if (websiteState.playerFullSize) {
+            if (fullSizeRef.current === null || fullSizeImgRef.current === null)
+                return
+
             refClassList.add('active--full-size--ref-container')
-            playContainerRef.current.style.height = '100vh'
-            playContainerRef.current.style.backgroundImage = `url(${playingTrack!.album.images[0].url})`
+            playerParentRef.current.style.height = '100vh'
+            playerParentRef.current.style.backgroundColor = 'var(--nl-clr-2)'
+
+            playerContainerRef.current.style.backgroundColor = 'transparent'
+
+            if (isMouseMoving) {
+                playerContainerRef.current.style.opacity = '1'
+                fullSizeRef.current.style.marginTop = '0'
+                fullSizeImgRef.current.style.width = '420px'
+            } else {
+                playerContainerRef.current.style.opacity = '0'
+                fullSizeRef.current.style.marginTop = '500px'
+                fullSizeImgRef.current.style.width = '640px'
+            }
         } else {
             refClassList.remove('active--full-size--ref-container')
-            playContainerRef.current.style.height = '120px'
-            playContainerRef.current.style.backgroundImage = 'unset'
+            playerParentRef.current.style.height = '120px'
+            playerParentRef.current.style.backgroundColor = 'unset'
+
+            playerContainerRef.current.style.backgroundColor = 'var(--nl-clr-2)'
+            playerContainerRef.current.style.opacity = '1'
         }
-    }, [websiteState.playerFullSize])
+    }, [websiteState.playerFullSize, isMouseMoving])
 
     return (
         playingTrack ?
-            <div ref={playContainerRef}>
-                <div id='player-container'>
+            <div id='player--parent-container' ref={playerParentRef}>
+                {
+                    websiteState.playerFullSize ?
+                        <div id='full-size--information-container' ref={fullSizeRef}>
+                            <img id='full-size--img' ref={fullSizeImgRef} src={playingTrack.album.images[0].url}
+                                 alt='track cover'/>
+                            <div>
+                                <h1 style={{fontSize: '58px'}}>{playingTrack.name}</h1>
+                                <h2 className='fs-pr-1 clr-sc-1 fw--semi-bold'>{artists}</h2>
+                            </div>
+                        </div>
+                        : null
+                }
+                <div id='player-container' ref={playerContainerRef}>
                     <div className='controls' style={{justifySelf: 'flex-start', gridArea: 'left'}}>
                         <FavouriteButton id='player-container--favourite' itemId={playingTrack.id}
                                          itemType={playingTrack.type} displayContent='image'/>
                         <FullSizeButton/>
-                        <FavouriteButton id='player-container--favourite' itemId={playingTrack.id}
-                                         itemType={playingTrack.type} displayContent='image'/>
                     </div>
 
                     <div className='controls' style={{gridArea: 'middle'}}>
