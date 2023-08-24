5344e48933e67e78d15998722d02cd9de3063022
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2023-08-24 16:43:04 +0200

    Update track.ts

--- src/data/data_types/track.ts
+++ src/data/data_types/track.ts
@@ -1,4 +1,5 @@
 export type Track = {
+    added_at: string
     id: string
     is_local: boolean
     name: string
@@ -11,12 +12,17 @@ export type Track = {
     uri: string
     album: {
         id: string
+        name: string
         images: { height: number; width: number; url: string }[]
         uri: string
     }
     artists: {
+        external_urls: {
+            spotify: string
+        }
         id: string
         uri: string
         name: string
+        type: string
     }[]
 }
